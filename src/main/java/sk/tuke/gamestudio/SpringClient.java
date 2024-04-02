63c5f0365f117734ebd64ae90c60d019768c83e4
Lukáš Proner <lukas.proner@student.tuke.sk>
2024-04-02 10:59:23 +0200

    8 cvicenie score

--- src/main/java/sk/tuke/gamestudio/SpringClient.java
+++ src/main/java/sk/tuke/gamestudio/SpringClient.java
@@ -2,18 +2,28 @@ package sk.tuke.gamestudio;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.game.numberlink.consoleui.ConsoleUI;
 import sk.tuke.gamestudio.service.*;
 
 @SpringBootApplication
 @Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
+        pattern = "sk.tuke.gamestudio.server.*"))
 public class SpringClient {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(SpringClient.class, args);
+    }*/
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringClient.class).web(WebApplicationType.NONE).run(args);
     }
 
     @Bean
@@ -28,7 +38,7 @@ public class SpringClient {
 
     @Bean
     public ScoreService scoreService(){
-        return new ScoreServiceJPA();
+        return new ScoreServiceRestClient();
     }
     @Bean
     public CommentService commentService() {
@@ -38,4 +48,9 @@ public class SpringClient {
     public RatingService ratingService() {
         return new RatingServiceJPA();
     }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
