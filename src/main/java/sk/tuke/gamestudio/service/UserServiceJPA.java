d3110b4b8d08bf8926763ad351a001e43abba2c1
Lukáš Proner <lukas.proner@student.tuke.sk>
2024-05-03 20:42:49 +0200

    pridal som hash metodu

--- src/main/java/sk/tuke/gamestudio/service/UserServiceJPA.java
+++ src/main/java/sk/tuke/gamestudio/service/UserServiceJPA.java
@@ -6,9 +6,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import sk.tuke.gamestudio.entity.Player;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Transactional
 public class UserServiceJPA implements UserService {
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -20,11 +23,19 @@ public class UserServiceJPA implements UserService {
 
     @Override
     public void addPlayer(Player player) {
+        // Získajte heslo hráča
+        String plainPassword = player.getPassword();
+
+        // Hashujte heslo pomocou BCrypt a nastavte hashované heslo hráčovi
+        String hashedPassword = passwordEncoder.encode(plainPassword);
+        player.setPassword(hashedPassword);
+
+        // Uložte hráča do databázy
         entityManager.persist(player);
     }
     @Override
     public boolean authenticate(String login, String password) {
         Player player = getPlayer(login);
-        return player != null && player.getPassword().equals(password);
+        return player != null && passwordEncoder.matches(password, player.getPassword());
     }
 }
